5a42f9f9441a80304d1b4d687bd6d07a79fad62f
akbarsahata <11588611+akbarsahata@users.noreply.github.com>
2020-11-09 03:04:07 +0700

    [MODIFY] route initiation will be develop here

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,3 +1,47 @@
-import { UserHandler } from './handlers'
+import { join } from 'path'
+import { FastifyInstance, FastifyPluginOptions } from 'fastify'
 
-export const user = UserHandler
+import { UserHandler } from './handlers'
+import { HandlerMetadataKeys, HTTPMethods } from '../libs/decorators/handler.decorator'
+
+export default async function api (instance: FastifyInstance, option: FastifyPluginOptions) {
+  Object.getOwnPropertyNames(
+    UserHandler.prototype
+  ).forEach((methodName: string) => {
+    if (methodName !== 'constructor' && typeof UserHandler.prototype[methodName] === 'function') {
+      const method: HTTPMethods = Reflect.getMetadata(
+        HandlerMetadataKeys.HTTPMethod,
+        UserHandler.prototype,
+        methodName
+      )
+      const routePrefix = Reflect.getMetadata(
+        HandlerMetadataKeys.RoutePrefix,
+        UserHandler.prototype
+      )
+      const routePath = Reflect.getMetadata(
+        HandlerMetadataKeys.RoutePath,
+        UserHandler.prototype,
+        methodName
+      )
+      const handlerFunc = UserHandler.prototype[methodName]
+
+      console.log(method, join(routePrefix, routePath), handlerFunc)
+
+      switch (method) {
+        case 'GET':
+          instance.get(
+            join(routePrefix, routePath),
+            handlerFunc
+          )
+          break
+        case 'POST':
+          instance.post(
+            join(routePrefix, routePath),
+            handlerFunc
+          )
+          break
+        default:
+      }
+    }
+  })
+}
