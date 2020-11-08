251cbf1610fc7f5967ee44638ad259582ce2f9e3
akbarsahata <11588611+akbarsahata@users.noreply.github.com>
2020-11-08 22:07:28 +0700

    [FIX] unreachable if run in docker problem

--- src/server.ts
+++ src/server.ts
@@ -6,7 +6,7 @@ server.get('/ping', async (request, reply) => {
   return 'pong\n'
 })
 
-server.listen(8080, (err, address) => {
+server.listen(8080, '0.0.0.0', (err, address) => {
   if (err) {
     console.error(err)
     process.exit(1)
