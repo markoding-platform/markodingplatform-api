4c16b7264ba3ba406e0321ba714241a9a9ee38f4
akbarsahata <11588611+akbarsahata@users.noreply.github.com>
2020-12-03 23:43:08 +0700

    [MODIFY] prepare for deploy

--- src/config/database.ts
+++ src/config/database.ts
@@ -3,7 +3,12 @@ import { createConnection, Connection } from "typeorm";
 
 import { User, Idea } from "../api/entity";
 
-const { POSTGRES_USER, POSTGRES_PASSWORD, POSTGRES_DB } = process.env;
+const {
+  POSTGRES_USER,
+  POSTGRES_PASSWORD,
+  POSTGRES_DB,
+  POSTGRES_URI,
+} = process.env;
 
 @Service()
 export default class Database {
@@ -18,7 +23,7 @@ export default class Database {
     this._connection = await createConnection({
       name: "connection-1",
       type: "postgres",
-      host: "d-markoding-postgres",
+      host: POSTGRES_URI,
       port: 5432,
       username: POSTGRES_USER,
       password: POSTGRES_PASSWORD,
--- src/index.ts
+++ src/index.ts
@@ -4,8 +4,10 @@ import { bootstrap } from "fastify-decorators";
 
 import IdeaController from "./api/controllers/idea.controller";
 
-const PORT = 8080;
-const ADDRESS = "0.0.0.0";
+const { APP_PORT, APP_HOST } = process.env;
+
+const PORT = APP_PORT || 8080;
+const ADDRESS = APP_HOST || "0.0.0.0";
 
 const server = fastify();
 
