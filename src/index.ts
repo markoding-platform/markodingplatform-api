554afcb0ac80662af1ed54e4bbd07fd45dd50059
Yofriadi Yahya <yofriadiyahya@gmail.com>
2020-12-03 18:44:09 +0700

    [FIX] route initialization error

--- src/index.ts
+++ src/index.ts
@@ -3,13 +3,11 @@ import fastify from "fastify";
 import { bootstrap } from "fastify-decorators";
 
 import IdeaController from "./api/controllers/idea.controller";
-// import routes from "./config/routes";
 
 const PORT = 8080;
 const ADDRESS = "0.0.0.0";
 
 const server = fastify();
-// server.register(routes);
 
 server.register(bootstrap, {
   controllers: [IdeaController],
