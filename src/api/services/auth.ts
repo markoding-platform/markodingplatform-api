faa4b76fbd13d49cb2f2d9e03a06f0df5ccfe9b4
akbarsahata <11588611+akbarsahata@users.noreply.github.com>
2020-12-05 03:38:42 +0700

    [ADD] generate and validate nonce

---
+++ src/api/services/auth.ts
@@ -0,0 +1,91 @@
+import crypto from "crypto";
+import querystring from "querystring";
+import jwt from "jsonwebtoken";
+import { Buffer } from "buffer";
+import { Initializer, Service } from "fastify-decorators";
+import { Repository } from "typeorm";
+import { addMinutes } from "date-fns";
+
+import Database from "../../config/database";
+import { Nonce, SSORequest, SSOResponse } from "../entity";
+
+const { NONCE_EXP_MIN, SSO_SECRET, JWT_SECRET } = process.env;
+
+@Service()
+class AuthService {
+  private repository!: Repository<Nonce>;
+
+  constructor(private database: Database) {}
+
+  @Initializer([Database])
+  async init(): Promise<void> {
+    this.repository = this.database.connection.getRepository(Nonce);
+  }
+
+  async generateNonce(): Promise<Nonce> {
+    const data = new Nonce();
+
+    data.expiredAt = addMinutes(new Date(), Number(NONCE_EXP_MIN));
+    data.nonce = crypto.randomBytes(16).toString("base64");
+
+    return this.repository.save(data);
+  }
+
+  async validateNonce(nonceStr: string): Promise<boolean> {
+    const nonce = await this.repository
+      .createQueryBuilder()
+      .where("nonce = :nonce AND expiredAt < :expiredAt", {
+        nonce: nonceStr,
+        expiredAt: new Date(),
+      })
+      .getOne();
+
+    return nonce !== undefined;
+  }
+
+  generateJWT(payload: any): string {
+    const token = jwt.sign(payload, JWT_SECRET as string);
+
+    return token;
+  }
+
+  async decodeJWT(jwtStr: string) {
+    return;
+  }
+
+  signNonce(nonceStr: string): SSORequest {
+    const rawPayload = `nonce=${nonceStr}`;
+    const payloadBase64 = Buffer.from(rawPayload, "utf-8").toString("base64");
+    const hmac = crypto.createHmac("SHA256", SSO_SECRET as string);
+
+    hmac.update(payloadBase64);
+
+    return {
+      sso: payloadBase64,
+      sig: hmac.digest("base64"),
+    };
+  }
+
+  verifySSO(ssoStr: string, sigStr: string): boolean {
+    const hmac = crypto.createHmac("SHA256", SSO_SECRET as string);
+
+    hmac.update(ssoStr);
+
+    return hmac.digest("base64") === sigStr;
+  }
+
+  decodeSSO(ssoStr: string): SSOResponse {
+    const converted = Buffer.from(ssoStr, "base64").toString("utf-8");
+    const decoded = querystring.parse(converted);
+
+    return {
+      nonce: decoded["nonce"] as string,
+      id: decoded["id"] as string,
+      email: decoded["email"] as string,
+      isEmailVerified: Boolean(decoded["isEmailVerified"] as string),
+      name: decoded["name"] as string,
+    };
+  }
+}
+
+export default AuthService;
