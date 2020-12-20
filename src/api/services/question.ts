43336c22bb323892b4a17732d00d064fe95ebca3
Ibnu Imam <ibnuimam@IT-KHD-MacBook-Pro.local>
2020-12-20 21:00:26 +0700

    [MODIFY] question service using alias

--- src/api/services/question.ts
+++ src/api/services/question.ts
@@ -20,7 +20,7 @@ export default class QuestionService {
     offset: number
   ): Promise<Question[]> {
     return this.repository
-      .createQueryBuilder()
+      .createQueryBuilder("Question")
       .where("channel_id = :channelId", {
         channelId,
       })
