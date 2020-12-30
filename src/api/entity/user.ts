9974c6a03a841c6c990d2d1d9a8c448cfc6a4dca
akbarsahata <11588611+akbarsahata@users.noreply.github.com>
2020-12-30 11:35:12 +0700

    checkpoint 1

--- src/api/entity/user.ts
+++ src/api/entity/user.ts
@@ -1,4 +1,8 @@
-import { Entity, PrimaryGeneratedColumn, Column } from "typeorm";
+import { Entity, PrimaryGeneratedColumn, Column, OneToMany } from "typeorm";
+
+import { Student } from "./student";
+
+export type UserType = "teacher" | "student" | "mentor";
 
 @Entity("users")
 export class User {
@@ -16,6 +20,16 @@ export class User {
 
   @Column({ default: false })
   isEmailVerified: boolean;
+
+  @Column({
+    type: "enum",
+    enum: ["teacher", "student", "mentor"],
+    nullable: true,
+  })
+  type?: UserType;
+
+  @OneToMany(() => Student, (student) => student.user)
+  student: Student[];
 }
 
 export type UserInput = Omit<User, "id">;
---
+++ src/libs/types/markoding-enum.d.ts
@@ -0,0 +1,19 @@
+declare module "markoding-enums" {
+  type JenjangInstansiEnum =
+    | "SD"
+    | "SMP"
+    | "SMA"
+    | "SMK"
+    | "SDLB"
+    | "SMPLB"
+    | "SMLB"
+    | "SLB"
+    | "Akademi"
+    | "Institut"
+    | "Sekolah Tinggi"
+    | "Politeknik"
+    | "Universitas"
+    | "BLK";
+
+  type JenjangTipeEnum = "Negeri" | "Swasta";
+}
