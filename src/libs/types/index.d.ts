c85518004908666f285be0d2016e859d872a1eff
Yofriadi Yahya <yofriadi.yahya@outlook.com>
2021-01-12 22:28:02 +0700

    fix(ideas): schema

---
+++ src/libs/types/index.d.ts
@@ -0,0 +1,12 @@
+export type ChatType = 'text' | 'image';
+export type ProfileType = 'student' | 'teacher' | 'mentor' | 'supporter';
+export enum SolutionType {
+  WEB = 'web',
+  MOBILE = 'mobile',
+  GAME = 'game',
+}
+export enum IdeaStatus {
+  PARTICIPANT = 'peserta',
+  FINALIST = 'finalis',
+  WINNER = 'pemenang',
+}
