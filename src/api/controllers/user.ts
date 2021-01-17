8e4f4cacf6cf6568b88479140270234f854045cc
akbarsahata <11588611+akbarsahata@users.noreply.github.com>
2021-01-17 15:12:24 +0700

    [MODIFY] follows refactor params on method getUserNotInTeam

--- src/api/controllers/user.ts
+++ src/api/controllers/user.ts
@@ -116,12 +116,13 @@ export default class UserController {
   async getUserMyStudentsNotInTeam(
     req: AuthenticatedRequest<{Querystring: CommonQueryString}>,
   ): Promise<User[]> {
-    const userTeachers = await this.service.getUserStudentNotInTeam(
-      req.query.search,
-      req.user.profile?.schoolId,
-      req.query.limit,
-      req.query.skip,
-    );
+    const userTeachers = await this.service.getUserStudentNotInTeam({
+      name: req.query.search,
+      schoolId: req.user.profile?.schoolId,
+      selfUserId: req.user.user.id,
+      limit: req.query.limit,
+      offset: req.query.skip,
+    });
 
     return userTeachers;
   }
